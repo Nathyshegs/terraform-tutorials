c6451661ff1c151989e7e317b26949f656a7fe73
Ajibola <101070743+Nathyshegs@users.noreply.github.com>
2023-05-06 07:03:17 -0400

    staging_commit

---
+++ infrastructure-live-v1/staging/vpc/0-provider.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+# For testing purposes, we will use the local backend which is the default option
+  backend "local" {
+    path = "staging/vpc/terraform.tfstate"   
+  }
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.62"
+    }
+  }
+}
